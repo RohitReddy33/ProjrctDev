c27b2c112a9b36752135d2b050c4b4d1bba9054d
RohithReddy33 <rohithreddy33333@gmail.com>
2021-02-09 19:17:07 +0530

    Add project files.

---
+++ TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ProjectDev
+{
+    [TestFixture]
+    [Category("UnitTests")]
+    class TestClass
+    {
+        [Test]
+        public void Test()
+        {
+            Class1 class1 = new Class1();
+            Assert.AreEqual("Test", class1.Test());
+        }
+    }
+}
